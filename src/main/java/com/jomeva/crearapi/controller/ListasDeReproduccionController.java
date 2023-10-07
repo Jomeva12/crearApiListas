96f8d03c355f9e7fd152cd66468487bad8eb806f
Jorge Melendez <ingwebmelendez@gmail.com>
2023-10-07 17:59:36 -0500

    Crud terminado en el Back y en el front

--- src/main/java/com/jomeva/crearapi/controller/ListasDeReproduccionController.java
+++ src/main/java/com/jomeva/crearapi/controller/ListasDeReproduccionController.java
@@ -1,97 +1,96 @@
-  /*
+/*
    * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
    * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-   */
-  package com.jomeva.crearapi.controller;
+ */
+package com.jomeva.crearapi.controller;
 
 import com.jomeva.crearapi.model.Cancion;
-  import com.jomeva.crearapi.model.ListasDeReproduccion;
-  import com.jomeva.crearapi.repository.ListaDeReproduccionService;
-  import com.jomeva.crearapi.repository.ListasDeReproduccionRepository;
-  import java.net.URI;
-  import java.util.List;
-  import java.util.Optional;
-  import org.springframework.beans.factory.annotation.Autowired;
-  import org.springframework.http.ResponseEntity;
-  import org.springframework.web.bind.annotation.CrossOrigin;
-  import org.springframework.web.bind.annotation.DeleteMapping;
-  import org.springframework.web.bind.annotation.GetMapping;
-  import org.springframework.web.bind.annotation.PathVariable;
-  import org.springframework.web.bind.annotation.PostMapping;
+import com.jomeva.crearapi.model.ListasDeReproduccion;
+import com.jomeva.crearapi.repository.ListaDeReproduccionService;
+import com.jomeva.crearapi.repository.ListasDeReproduccionRepository;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-  import org.springframework.web.bind.annotation.RequestBody;
-  import org.springframework.web.bind.annotation.RequestMapping;
-  import org.springframework.web.bind.annotation.RestController;
-  import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-  /**
-   *
-   * @author Jorge Melendez
-   */
-  @RestController
-  @RequestMapping("/lists")
-  public class ListasDeReproduccionController {
+/**
+ *
+ * @author Jorge Melendez
+ */
+@RestController
+@RequestMapping("/lists")
+public class ListasDeReproduccionController {
 
-    @Autowired
-    private ListaDeReproduccionService listaDeReproduccionService;
+  @Autowired
+  private ListaDeReproduccionService listaDeReproduccionService;
 
-   @PostMapping
-      public ResponseEntity<Object> crearListaDeReproduccion(@RequestBody ListasDeReproduccion listaDeReproduccion) {
-          if (listaDeReproduccion.getNombre() == null || listaDeReproduccion.getNombre().isEmpty()) {
-              return ResponseEntity.badRequest().body("El nombre de la lista no es válido");
-          }
-
-          ListasDeReproduccion nuevaLista = listaDeReproduccionService.createListasDeReproduccion(listaDeReproduccion);
-          URI location = ServletUriComponentsBuilder
-                  .fromCurrentRequest()
-                  .path("/{id}")
-                  .buildAndExpand(nuevaLista.getId())
-                  .toUri();
-
-          return ResponseEntity.created(location).body(nuevaLista);
-      }
-
-    @GetMapping
-    public List<ListasDeReproduccion> getAllListasDeReproduccion() {
-      return listaDeReproduccionService.getAllListasDeReproduccion();
+  @PostMapping
+  public ResponseEntity<Object> crearListaDeReproduccion(@RequestBody ListasDeReproduccion listaDeReproduccion) {
+    if (listaDeReproduccion.getNombre() == null || listaDeReproduccion.getNombre().isEmpty()) {
+      return ResponseEntity.badRequest().body("El nombre de la lista no es válido");
     }
 
+    ListasDeReproduccion nuevaLista = listaDeReproduccionService.createListasDeReproduccion(listaDeReproduccion);
+    URI location = ServletUriComponentsBuilder
+            .fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(nuevaLista.getId())
+            .toUri();
 
-    @GetMapping("/get/{listName}")
-      public ResponseEntity<Object> obtenerListaDeReproduccionPorNombre(@PathVariable String listName) {
-          ListasDeReproduccion lista = listaDeReproduccionService.obtenerListaDeReproduccionPorNombre(listName);
+    return ResponseEntity.created(location).body(nuevaLista);
+  }
 
-          if (lista != null) {
-              return ResponseEntity.ok(lista);
-          } else {
-              return ResponseEntity.notFound().build();
-          }
-      }
+  @GetMapping
+  public List<ListasDeReproduccion> getAllListasDeReproduccion() {
+    return listaDeReproduccionService.getAllListasDeReproduccion();
+  }
 
+  @GetMapping("/get/{listName}")
+  public ResponseEntity<Object> obtenerListaDeReproduccionPorNombre(@PathVariable String listName) {
+    ListasDeReproduccion lista = listaDeReproduccionService.obtenerListaDeReproduccionPorNombre(listName);
+
+    if (lista != null) {
+      return ResponseEntity.ok(lista);
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
 
   @DeleteMapping("/delete/{listName}")
   public ResponseEntity<String> deleteListaDeReproduccionPorNombre(@PathVariable String listName) {
-      // Llama al servicio para eliminar la lista de reproducción por nombre
-      boolean deleted = listaDeReproduccionService.deleteListaDeReproduccionPorNombre(listName);
+    // Llama al servicio para eliminar la lista de reproducción por nombre
+    boolean deleted = listaDeReproduccionService.deleteListaDeReproduccionPorNombre(listName);
 
-      if (deleted) {
-          return ResponseEntity.noContent().build(); // Devuelve 204 No Content si se eliminó correctamente
-      } else {    
-          return ResponseEntity.notFound().build(); // Devuelve 404 Not Found si la lista no existe
-      }
+    if (deleted) {
+      return ResponseEntity.noContent().build(); // Devuelve 204 No Content si se eliminó correctamente
+    } else {
+      return ResponseEntity.notFound().build(); // Devuelve 404 Not Found si la lista no existe
+    }
   }
 
-      @GetMapping("/{id}")
-    public void deleteListasDeReproduccionById(@PathVariable("id") Long id) {
-       listaDeReproduccionService.deleteListasDeReproduccion(id);
-    }
-    
-     @PutMapping("/lists/add/{playlistId}")
-  public ResponseEntity<Object> agregarCancionALista(@PathVariable Long playlistId,@RequestBody Cancion cancion)
-  {
-    // Busca la lista de reproducción por su ID
-    ListasDeReproduccion listaDeReproduccion = listaDeReproduccionService.getListasDeReproduccion(playlistId);
+  @GetMapping("/{id}")
+  public void deleteListasDeReproduccionById(@PathVariable("id") Long id) {
+    listaDeReproduccionService.deleteListasDeReproduccion(id);
+  }
 
+  @PutMapping("/add/{playlistId}")
+  public ResponseEntity<Object> agregarCancionALista(@PathVariable Long playlistId, @RequestBody Cancion cancion) {
+    // Busca la lista de reproducción por su ID
+    System.out.println("-----> "+cancion);
+    ListasDeReproduccion listaDeReproduccion = listaDeReproduccionService.getListasDeReproduccion(playlistId);
+    Long idCancion = cancion.getId();
+    System.out.println("-----> "+idCancion);
     if (listaDeReproduccion == null) {
       return ResponseEntity.notFound().build();
     }
@@ -103,11 +102,11 @@ import org.springframework.web.bind.annotation.PutMapping;
     listaDeReproduccion = listaDeReproduccionService.createListasDeReproduccion(listaDeReproduccion);
 
     URI location = ServletUriComponentsBuilder
-        .fromCurrentRequest()
-        .path("/{id}")
-        .buildAndExpand(listaDeReproduccion.getId())
-        .toUri();
+            .fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(listaDeReproduccion.getId())
+            .toUri();
 
     return ResponseEntity.created(location).body(listaDeReproduccion);
   }
-  }
+}
--- src/main/java/com/jomeva/crearapi/model/ListasDeReproduccion.java
+++ src/main/java/com/jomeva/crearapi/model/ListasDeReproduccion.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.List;
@@ -23,7 +24,7 @@ public class ListasDeReproduccion {
     private String nombre;
     private String descripcion;
 
-    @OneToMany
+    @ManyToMany
     private List<Cancion> canciones;
 
    
